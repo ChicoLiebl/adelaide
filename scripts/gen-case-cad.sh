26a10e3eb1ea7c0705d6e42e4023581aea46d448
chicoliebl <chicolliebl@gmail.com>
2025-09-09 22:32:27 -0300

    update lib for correct cad generation

--- scripts/gen-case-cad.sh
+++ scripts/gen-case-cad.sh
@@ -9,6 +9,7 @@ OUT_DIR="$BASE_DIR/FAB-OUTPUTS/$PROJECT_NAME-case-$(date +%y-%m-%d)-$GIT_VERSION
 
 mkdir -p $OUT_DIR
 
-kicad-cli pcb export svg --layers "F.Courtyard,Edge.Cuts" --drill-shape-opt 0 --cl "" --mode-single --exclude-drawing-sheet  -o "$OUT_DIR/top-plate.svg" $PCB_FILE
-kicad-cli pcb export svg --layers "B.Courtyard,Edge.Cuts" --drill-shape-opt 0 --cl "" --mode-single --exclude-drawing-sheet  -o "$OUT_DIR/bottom-plate.svg" $PCB_FILE
-kicad-cli pcb export svg --layers "User.Eco1,Edge.Cuts" --drill-shape-opt 0 --cl "" --mode-single --exclude-drawing-sheet  -o "$OUT_DIR/spacer.svg" $PCB_FILE
+kicad-cli pcb export svg --layers "F.Courtyard,Edge.Cuts" --drill-shape-opt 0 --cl "" --mode-single --exclude-drawing-sheet  -o "$OUT_DIR/top-spacer.svg" $PCB_FILE
+kicad-cli pcb export svg --layers "User.Eco1,Edge.Cuts" --drill-shape-opt 0 --cl "" --mode-single --exclude-drawing-sheet  -o "$OUT_DIR/top-plate.svg" $PCB_FILE
+kicad-cli pcb export svg --layers "B.Courtyard,Edge.Cuts" --drill-shape-opt 0 --cl "" --mode-single --exclude-drawing-sheet  -o "$OUT_DIR/bottom-spacer.svg" $PCB_FILE
+kicad-cli pcb export svg --layers "User.Eco1,Edge.Cuts" --drill-shape-opt 0 --cl "" --mode-single --exclude-drawing-sheet  -o "$OUT_DIR/holes.svg" $PCB_FILE
