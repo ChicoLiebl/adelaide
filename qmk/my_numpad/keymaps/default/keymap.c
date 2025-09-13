c3212b4ab85b171d8a78e394df67ee39cd99aac5
chicoliebl <chicolliebl@gmail.com>
2025-09-13 20:04:22 -0300

    fixed qmk configuration

--- qmk/my_numpad/keymaps/default/keymap.c
+++ qmk/my_numpad/keymaps/default/keymap.c
@@ -5,47 +5,78 @@
 
 #define _BL 0
 #define _FN 1
+#define _FN_BOOT 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
-     * │ - │ * │ / │Fn │
+     * │Fn │ / │ * │ - │
      * ├───┼───┼───┼───┤
-     * │   │ 9 │ 8 │ 7 │
-     * | + ├───┼───┼───┤
-     * │   │ 6 │ 5 │ 4 │
+     * │ 7 │ 8 │ 9 │   │
+     * ├───┼───┼───┤ + |
+     * │ 4 │ 5 │ 6 │   │
      * ├───┼───┼───┼───┤
-     * │   │ 3 │ 2 │ 1 │
-     * |Ent├───┼───┴───┤
-     * │   │Del│   0   |
-     * └───┴───┴───────┘
+     * │ 1 │ 2 │ 3 │   │
+     * ├───┴───┼───┤Ent|
+     * │   0   | . |   |
+     * └───┴───┴───┴───┘
      */
     [_BL] = LAYOUT_numpad_5x4(
-        KC_PMNS, KC_PAST, KC_PSLS, MO(_FN),
-                 KC_P9,   KC_P8,   KC_P7,
-        KC_PPLS, KC_P6,   KC_P5,   KC_P4,
-                 KC_P3,   KC_P2,   KC_P1,
-        KC_PENT, KC_PDOT, KC_P0
+        MO(_FN), KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,   KC_P2,   KC_P3,
+        KC_P0,            KC_PDOT, KC_PENT
     ),
 
-    /*
-     * ┌───┬───┬───┬───┐
-     * │Rst│ * │ / │Fn │
-     * ├───┼───┼───┼───┤
-     * │   │ 9 │ ↑ │ 7 │
-     * | + ├───┼───┼───┤
-     * │   │ ← │ 5 │ → │
-     * ├───┼───┼───┼───┤
-     * │   │ 3 │ ↓ │ 1 │
-     * |Ent├───┼───┴───┤
-     * │   │Del│   0   |
-     * └───┴───┴───────┘
+    /* Function Layer: Backlight and Numlock
+     * ,---------------.
+     * |FN |TOG|BRE|   | toggle / breathing 
+     * |---+---+---|---|
+     * |   |   |   |   |  
+     * |---+---+---|RET|
+     * |   |   |   |   | 
+     * |---+---+---|---|
+     * |   |B- |B+ |NUM| Brightness
+     * |---+---+---|   |
+     * |FN_BOOT|   |LCK|
+     * `---------------'
      */
     [_FN] = LAYOUT_numpad_5x4(
-        QK_BOOT, KC_PAST, KC_PSLS, MO(_FN),
-                 KC_P9,   KC_UP,   KC_P7,
-        KC_PPLS, KC_LEFT, KC_P5,   KC_RGHT,
-                 KC_P3,   KC_DOWN, KC_P1,
-        KC_PENT, KC_PDOT, KC_P0
+        KC_TRNS, BL_TOGG, KC_NO, KC_NO,
+        KC_NO,   KC_NO, KC_NO,
+        KC_NO,   BL_BRTG, KC_NO, KC_RETN,
+        KC_NO,   BL_DOWN, BL_UP,
+        MO(_FN_BOOT),     KC_NO, KC_NUM
     ),
+
+    /* Function Layer 2: Boot mode
+     * ,---------------.
+     * |FN |   |   |   |
+     * |---+---+---|---|
+     * |   |   |   |   |  
+     * |---+---+---|   |
+     * |   |   |   |   | 
+     * |---+---+---|---|
+     * |   |   |   |   |
+     * |---+---+---|BO-|
+     * |FN_BOOT|   |OT |
+     * `---------------'
+     */
+    [_FN_BOOT] = LAYOUT_numpad_5x4(
+        KC_TRNS, KC_NO, KC_NO, KC_NO,
+        KC_NO,   KC_NO, KC_NO,
+        KC_NO,   KC_NO, KC_NO, KC_NO,
+        KC_NO,   KC_NO, KC_NO,
+        KC_TRNS,        KC_NO, QK_BOOT
+    ),
+
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_BL] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_FN] =  { ENCODER_CCW_CW(BL_DOWN, BL_UP)},
+    [_FN_BOOT] =  { ENCODER_CCW_CW(BL_DOWN, BL_UP)},
+};
+#endif
